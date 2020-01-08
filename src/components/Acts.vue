403c279c94ed05bebdfcc038bca2f3524b94f65d
sc11p <evtmail@gmail.com>
2020-01-08 06:44:30 -0800

    acts datatables

--- src/components/Acts.vue
+++ src/components/Acts.vue
@@ -1,25 +1,36 @@
 <template>
-    <div>
-        <div v-for="act in acts" v-bind:key="act.id">
-            <p>{{ act.id }}</p>
-            <p>{{ act.date }}</p>
-        </div>
-    </div>
+  <v-data-table
+    :headers="headers"
+    :items="content"
+    :items-per-page="5"
+    class="elevation-1"
+  >
+  <template slot="items" slot-scope="props">
+        <td>{{ props.item.id }}</td>
+        <td class="text-xs-right">{{ props.item.id }}</td>
+  </template>
+  
+  </v-data-table>
 </template>
 
 <script>
-    import apiRoutes from "../plugins/apiRoutes";
-    export default {
-        name: "Acts",
-        data() {
-            return {
-                acts: ''
+  import apiRoutes from "../plugins/apiRoutes";
+  export default {
+      name: "Acts",
+      data() {
+          return {
+            headers: [
+            { text: 'ids', value: 'name' },
+            { text: 'data', value: 'name' },
+            ],
+            content: [],
+            beforeCreate() {
+                this.$axios.get(apiRoutes.acts).then(response => this.content = response.data);
             }
-        },
-        beforeCreate() {
-            this.$axios.get(apiRoutes.acts).then(response => this.acts = response.data);
-        }
-    }
+          }
+      }
+  }
+
 </script>
 
 <style scoped>
